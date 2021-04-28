e6cde4390d04d45d8e976b8a0d46bfc7a35ac8ae
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-04-28 16:15:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function(){
+  let p = document.getquerySelector("#text").innerText="This is very cool"
   console.log("The DOM has loaded");
+  
 });
 console.log("jsdfjpasjo")
